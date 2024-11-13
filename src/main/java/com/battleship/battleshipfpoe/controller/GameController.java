bd700d8461adbc1cd1788204d019f83cda5cc70d
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2024-11-12 21:12:03 -0500

    Configuracion del controlador welcome

--- src/main/java/com/battleship/battleshipfpoe/controller/GameController.java
+++ src/main/java/com/battleship/battleshipfpoe/controller/GameController.java
@@ -1,4 +1,12 @@
 package com.battleship.battleshipfpoe.controller;
 
+import com.battleship.battleshipfpoe.view.GameStage;
+import com.battleship.battleshipfpoe.view.WelcomeStage;
+
 public class GameController {
+
+    public void handleClickExit(){
+        GameStage.deleteInstance();
+        WelcomeStage.getInstance();
+    }
 }
