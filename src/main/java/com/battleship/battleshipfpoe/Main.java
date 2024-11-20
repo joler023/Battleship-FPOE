7f145a98ea58823bd85b29652e305f30fc31ace9
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2024-11-19 22:08:07 -0500

    Metodo que reconoce las casillas presionadas

--- src/main/java/com/battleship/battleshipfpoe/Main.java
+++ src/main/java/com/battleship/battleshipfpoe/Main.java
@@ -14,6 +14,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         GameStage.getInstance();
-        WelcomeStage.getInstance();
+        //WelcomeStage.getInstance();
     }
 }
